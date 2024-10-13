bf04765bb66abd714dd75a8f0de92e6d28d8890d
Daniel <dgirotto00@gmail.com>
2024-10-13 06:29:16 -0300

    Preparação para deploy no Heroku

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/danielgirotto/Projetos/TesteMayo/venv"
+set -gx VIRTUAL_ENV "/home/danielgirotto/Projetos/todo-teste/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
